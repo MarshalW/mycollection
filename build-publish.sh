a4c045c716ebf542b5f50430e13b34e40ff665df
Marshal Wu <marshal.wu@gmail.com>
2021-08-26 15:48:39 +0800

    delete temp file and complete build publish shell script

--- build-publish.sh
+++ build-publish.sh
@@ -2,9 +2,22 @@
 
 set -e
 
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+if [[ -z "${ANSIBLE_GALAXY_TOKEN}" ]]; then
+  echo "warning: ANSIBLE_GALAXY_TOKEN unset"
+fi
+
 TAG=$1
 
 sed -i "s|^version: .*|version: $TAG|" galaxy.yml
+
+git add galaxy.yml
+git commit -m "change version to $TAG"
 
 git push
 git tag "v$TAG"
@@ -12,6 +25,6 @@ git push origin "v$TAG"
 
 ansible-galaxy collection build
 
-ansible-galaxy collection publish ./marshalw-mycollection-$TAG.tar.gz --token='{{ lookup('env', 'ANSIBLE_GALAXY_TOKEN') }}'
+ansible-galaxy collection publish ./marshalw-mycollection-$TAG.tar.gz --token=$ANSIBLE_GALAXY_TOKEN
 
 
